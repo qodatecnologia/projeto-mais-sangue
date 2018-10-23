849d2251f2732cb44b216acf6c559e9bc6623d51
Washington Alexandre <was.alexandre42@gmail.com>
2018-10-23 18:12:15 -0300

    alterado projeto de html para 'angular'

---
+++ frontend-angular/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ThfModule } from '@totvs/thf-ui';
+import { CommonModule } from '@angular/common';
+
+import { AgmCoreModule } from '@agm/core';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    ThfModule,
+    RouterModule.forRoot([]),
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAnxIUEmOAwodzjRwJtyncOeopUSfG9MyA'
+    })    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
