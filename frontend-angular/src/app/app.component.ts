55ea394dfdc2655a2448813a4cf5b1d7754fedb1
Washington Alexandre <was.alexandre42@gmail.com>
2018-10-25 22:49:59 -0300

    Adicionado modal de confirmação de envio

--- frontend-angular/src/app/app.component.ts
+++ frontend-angular/src/app/app.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { DonorService } from './services/donor.service';
 import { ReceptorService } from './services/receptor.service';
 import { Donor } from './shared/donor';
 import { Receptor } from './shared/receptor';
+
+import { ThfModalComponent } from '@totvs/thf-ui/components/thf-modal/thf-modal.component';
+import { ThfModalAction } from '@totvs/thf-ui/components/thf-modal/thf-modal-action.interface';
 
 @Component({
   selector: 'app-root',
@@ -11,23 +14,36 @@ import { Receptor } from './shared/receptor';
 })
 export class AppComponent {
 
-  constructor(
-    private donorService: DonorService,
-    private receptorService: ReceptorService
-  ){ }
-
   donor: Donor = new Donor();
   receptor: Receptor = new Receptor();
 
   title = 'mais-sangue';
   lat: number = 51.678418;
-  lng: number = 7.809007;
+  lng: number = 7.809007;  
 
+  @ViewChild('formModal') formModal: ThfModalComponent;
+
+  public readonly modalPrimaryAction: ThfModalAction = {
+    action: () => this.returnToHome(),
+    label: 'Fechar'
+  };  
+
+  constructor(
+    private donorService: DonorService,
+    private receptorService: ReceptorService
+  ){ }
+
+  returnToHome() {
+    this.donor = new Donor();
+    this.receptor = new Receptor();   
+    this.formModal.close() 
+  }
 
   sendDonor() {
     this.donorService.post(this.donor).subscribe(
       data => {
         console.log("POST Request is successful ", data);
+        this.formModal.open();
     },
     error => {
         console.log("Error", error);
@@ -39,6 +55,7 @@ export class AppComponent {
     this.receptorService.post(this.receptor).subscribe(
       data => {
         console.log("POST Request is successful ", data);
+        this.formModal.open();
     },
     error => {
         console.log("Error", error);
